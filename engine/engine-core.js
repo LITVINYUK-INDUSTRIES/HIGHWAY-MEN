639e9889d2b571cc1c4b3933269a6a3f6cb35304
LITVINYUK INDUSTRIES <legoandrew321@gmail.com>
2026-02-20 11:11:49 -0700

    Update engine-core.js

--- engine/engine-core.js
+++ engine/engine-core.js
@@ -1,1 +1,25 @@
+import { initRenderer } from './engine-renderer.js';
+import { initScene } from './engine-scene.js';
+import { initCamera } from './engine-camera.js';
+import { startLoop } from './engine-loop.js';
+import { Vessel } from '../systems/vessel-class.js';
+import { loadSectorEntities } from '../systems/vessel-loader.js';
+import { WorldState } from '../systems/vessel-registry.js';
 
+export let scene, camera, renderer;
+
+export function bootEngine() {
+    console.warn("ENGINE_START_SEQUENCE");
+
+    scene = initScene();
+    camera = initCamera();
+    renderer = initRenderer();
+
+    // Spawn local player
+    new Vessel({ id: WorldState.localPlayerId, p: [0, 0.35, 0], r: [0] }, scene);
+
+    // Spawn ghost vehicles
+    loadSectorEntities(scene);
+
+    startLoop(scene, camera, renderer);
+}
